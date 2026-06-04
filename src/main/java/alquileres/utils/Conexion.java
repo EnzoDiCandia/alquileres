93ea40f1697115f0223fbc072d2ced4cbebb6ced
Enzo Di Candia <enzo.lp89@gmail.com>
2026-06-04 15:37:32 -0300

    leer conexion desde variables de entorno

--- src/main/java/alquileres/utils/Conexion.java
+++ src/main/java/alquileres/utils/Conexion.java
@@ -5,11 +5,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexion {
-    private static final String URL = "jdbc:mysql://localhost:3306/alquileres_db";
-    private static final String USER = "root";
-    private static final String PASSWORD = "root";
 
     public static Connection getConexion() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        String url = System.getenv("SPRING_DATASOURCE_URL");
+        String user = System.getenv("SPRING_DATASOURCE_USERNAME");
+        String password = System.getenv("SPRING_DATASOURCE_PASSWORD");
+
+        if (url == null) url = "jdbc:mysql://localhost:3306/alquileres_db?sslMode=REQUIRED&trustServerCertificate=true";
+        if (user == null) user = "root";
+        if (password == null) password = "root";
+
+        return DriverManager.getConnection(url, user, password);
     }
 }
