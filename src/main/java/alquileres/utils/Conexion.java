abbdda736766c87338f601df83c148816c47e9e8
Enzo Di Candia <enzo.lp89@gmail.com>
2026-06-08 17:59:01 -0300

    fix variables de entorno

--- src/main/java/alquileres/utils/Conexion.java
+++ src/main/java/alquileres/utils/Conexion.java
@@ -11,7 +11,11 @@ public class Conexion {
         String user = System.getenv("SPRING_DATASOURCE_USERNAME");
         String password = System.getenv("SPRING_DATASOURCE_PASSWORD");
 
-        if (url == null) url = "jdbc:mysql://localhost:3306/alquileres_db?sslMode=REQUIRED&trustServerCertificate=true";
+        if (url == null) url = System.getProperty("SPRING_DATASOURCE_URL");
+        if (user == null) user = System.getProperty("SPRING_DATASOURCE_USERNAME");
+        if (password == null) password = System.getProperty("SPRING_DATASOURCE_PASSWORD");
+
+        if (url == null) url = "jdbc:mysql://localhost:3306/alquileres_db?trustServerCertificate=true";
         if (user == null) user = "root";
         if (password == null) password = "root";
 
